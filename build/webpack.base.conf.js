f5d3db491679ce49e8693ba936f92f84954d0386
Annilla <anny09117011@gmail.com>
2017-04-09 15:48:16 +0800

    reset webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,8 +22,7 @@ module.exports = {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
-      'assets': path.resolve(__dirname, '../src/assets'),
+      '@': resolve('src')
     }
   },
   module: {
